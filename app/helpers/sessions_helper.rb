f67b74558b44233cf67caf946ddf31f4f1e22b8e
Joel Thompson <joel.thompson@intercom.io>
2017-06-22 14:07:52 -0700

    adding account dropdown to header

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # returns the currently logged in user if they exist
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
 end
