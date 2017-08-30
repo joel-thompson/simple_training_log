ac0eb5eb62d5884032a4de3c7d14b4872dd6e588
Joel Thompson <joel.thompson@intercom.io>
2017-08-30 15:07:51 -0700

    added some authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,6 +33,11 @@ module SessionsHelper
 
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
