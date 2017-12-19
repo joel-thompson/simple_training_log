da5323b56c5a7b99e8d0a229a431db309db574e8
Joel Thompson <joel.thompson@intercom.io>
2017-12-19 12:52:37 -0800

    fixing up how I handle occurred (#32)

--- spec/lib/entries_spec.rb
+++ spec/lib/entries_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe MartialArts do
 
   before do
-    @options_friendly = ['Now', 'Morning', 'Afternoon', 'Evening']
-    @options = ['now', 'morning', 'afternoon', 'evening']
+    @options_friendly = ['Morning', 'Afternoon', 'Evening']
+    @options = ['morning', 'afternoon', 'evening']
     @times = []
     @options_friendly.zip(@options).each do |a, b|
       @times << [a,b]
@@ -25,6 +25,35 @@ describe MartialArts do
     result_lib = Entries.date_and_time_handler(date, time)
 
     expect(result_lib).to eq result_control
+  end
+
+  describe "#get_occurred_string" do
+    it "returns the evening string correctly" do
+      zone = Time.now.to_s.split(" ")[2]
+      control = ['2017-6-5', "18:00:00", zone].join(' ')
+      occurred_at = DateTime.parse(control)
+
+      result = Entries.get_occurred_string(occurred_at)
+      expect(result).to eq('evening')
+    end
+  end
+
+  it "returns the afternoon string correctly" do
+    zone = Time.now.to_s.split(" ")[2]
+    control = ['2017-6-5', "13:00:00", zone].join(' ')
+    occurred_at = DateTime.parse(control)
+
+    result = Entries.get_occurred_string(occurred_at)
+    expect(result).to eq('afternoon')
+  end
+
+  it "returns the morning string correctly" do
+    zone = Time.now.to_s.split(" ")[2]
+    control = ['2017-6-5', "07:00:00", zone].join(' ')
+    occurred_at = DateTime.parse(control)
+
+    result = Entries.get_occurred_string(occurred_at)
+    expect(result).to eq('morning')
   end
 
 end
