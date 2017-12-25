ba4f70f16158d8be0d268600356002643849c083
Joel Thompson <joel.thompson@intercom.io>
2017-12-25 07:40:37 -0800

    styling in a good place, title work starts next (#37)
    visual clean up

--- app/helpers/martial_arts/martial_arts_helper.rb
+++ app/helpers/martial_arts/martial_arts_helper.rb
@@ -2,4 +2,16 @@ module MartialArts::MartialArtsHelper
   def form_path
     @martial_art.id.present? ? martial_art_path(@martial_art) : martial_arts_path
   end
+
+  def time_and_date_text
+    return "" unless @martial_art.present?
+
+    @martial_art.occurred_date.to_s(:short_ordinal) +
+      " - " +
+      @martial_art.occurred_time.capitalize      
+  end
+
+  def duration_text
+    pluralize(@martial_art.duration_in_seconds / 60, "minute")
+  end
 end
