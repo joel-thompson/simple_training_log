8b8ab44abd0f1a271b31886106e52b679c1236c9
Joel Thompson <joel.thompson@intercom.io>
2018-02-13 17:58:25 -0800

    add an event for removing weight record (#67)

--- app/controllers/body_weight_records_controller.rb
+++ app/controllers/body_weight_records_controller.rb
@@ -9,7 +9,7 @@ class BodyWeightRecordsController < ApplicationController
   def create
     @record = current_user.body_weight_records.new(weight: params[:weight])
     if @record.save
-      record_intercom_event
+      record_intercom_event('Updated Body Weight')
       flash[:success] = "Saved!"
       redirect_to root_url
     else
@@ -19,6 +19,7 @@ class BodyWeightRecordsController < ApplicationController
   end
 
   def destroy
+    record_intercom_event('Deleted Body Weight Record')
     @record.destroy
     flash[:success] = "Deleted"
     redirect_back(fallback_location: root_url)
@@ -29,10 +30,10 @@ class BodyWeightRecordsController < ApplicationController
     redirect_to root_url if @record.nil?
   end
 
-  private def record_intercom_event
+  private def record_intercom_event(name)
     RecordIntercomEventWorker.perform_async(
       current_user.id,
-      'Updated Body Weight',
+      name,
       { weight: @record.weight },
     )
   end
