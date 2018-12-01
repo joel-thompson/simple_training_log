bec5aa485960b43359dc1a91d833e81a87047ee9
Joel Thompson <joel.thompson@intercom.io>
2018-12-01 11:25:53 -0800

    add program schedule to homepage (#87)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
       @feed_items = current_user.all_entries.paginate(page: params[:page], per_page: 10)
       @lift_choices = current_user.lift_choices
       @cardio_choices = current_user.cardio_choices
+      @current_program = current_user&.active_training_program&.decorate
     end
   end
 end
