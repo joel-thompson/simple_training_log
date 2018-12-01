763f37e7d24278f30dab83febcf5d984ebc49f35
chuckwalla <jeelthompson@gmail.com>
2018-12-01 11:35:23 -0800

    make it clear if im in dev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
 	  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "LP"
+    base_title = if Rails.env.development?
+      "DEV LP"
+    else
+      "LP"
+    end
+    
     if page_title.empty?
       base_title
     else
